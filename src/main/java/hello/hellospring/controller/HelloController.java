d4aab6e7515d27e7718cfe70685b7fb936e94006
1yangsh <ysh410@gmail.com>
2021-12-15 17:19:26 +0900

    Add spring project init

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,22 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")  // url endpoint
+    public String hello(Model model) {
+        model.addAttribute("data", "spring!!!!");
+        return "hello"; // template의 html 파일명
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+}
